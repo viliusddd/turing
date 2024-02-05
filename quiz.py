0ef02a98460bcb75e1235bf8ac41eefad0641b80
Vilius <vilius@vilius.net>
2024-02-05 17:57:38 +0200

    Add Quiz class,  improve  question args

--- quiz.py
+++ quiz.py
@@ -7,19 +7,17 @@ options to manage the questions.
 
 Usage:
   ./quiz.py [options]
-  ./quiz.py test|practice|question|status
-  ./quiz.py test
-  ./quiz.py practice
+  ./quiz.py test|practice|question|stats|reset
+  ./quiz.py stats [--id=<id>|--user=<username>] [--status=<status> --type=<type>]
   ./quiz.py profile [<username>|--user=<username>|--add-user=<username>|--remove-user=<username>]
-  ./quiz.py question [<id>|--toggle-status <id>|--remove <id>]
-  ./quiz.py status [--id=<id>|--user=<username>] [--status=<status> --type=<type>]
+  ./quiz.py question [--enabled|--remove|--update|--reset] <id>
 
 
 Commands:
   test              Test mode
   practice          Practice mode
   question          Questions editing mode
-  status            Show statistics of questions
+  stats             Show statistics of questions
   profile           Create/change/see status of profile
 
 Options:
@@ -31,8 +29,10 @@ Options:
   --all             Show all questions
   --active          Show active questions
   --inactive        Show inactive questions
-  --status          Choose to show only active or inactive questions
+#   --status          Choose to show only active or inactive questions
   --type            Choose to show free-form or multi-choice questions
+  -v --verbose      Print verbose output to terminal: Print explanations
+  -vv               Print very verbose output to terminal: print explanations and output tables
 '''
 
 import csv
@@ -42,153 +42,225 @@ from docopt import docopt
 from tabulate import tabulate
 
 
-def get_db(filename):
-    with open(filename) as file:
-        reader = list(csv.DictReader(file))
-        for row in reader:
-            row['id'] = int(row['id'])
-            row['times_shown'] = int(row['times_shown'])
-            row['correct'] = int(row['correct'])
+class Quiz:
+    def __init__(self, filename):
+        self.filename = filename
+        self.db = []
+        self._get_db()
 
-    return reader
+    def _get_db(self):
+        with open(self.filename) as file:
+            self.db = list(csv.DictReader(file))
+            for row in self.db:
+                row['_id'] = int(row['_id'])
+                row['times_shown'] = int(row['times_shown'])
+                row['correct'] = int(row['correct'])
 
+                if row['enabled'] == "True":
+                    row['enabled'] = True
+                else:
+                    row['enabled'] = False
 
-def append_db(row, filename='db1_short.csv'):
-    # generate header
-    with open(filename) as file:
-        header = list(file)[0]
-        print('header: ', header)
+    def _save_db(self):
+        with open(self.filename, 'w') as file:
+            writer = csv.DictWriter(file, fieldnames=[i for i in self.db[0]])
+            writer.writeheader()
+            writer.writerows(self.db)
 
-    with open(filename, 'a', newline='') as file:
-        writer = csv.DictWriter(file, fieldnames=header)
+    def freeform_question(self):
+        for i, row in enumerate(self.db):
+            print('-' * 80)
+            print(f'{i + 1}. {row["question"]}')
+            user_answer = input('Your answer: ')
+            if user_answer == row['answer']:
+                print('Success! Your answer is correct!')
+                row['correct'] += 1
+                row['times_shown'] += 1
+            else:
+                print(f'You are wrong. Correct answer: {row["answer"]}')
+                row['times_shown'] += 1
 
-        print(row)
-        writer.writerow(row)
+        self._save_db()
 
+    def quiz_question(self):
+        """
+        TODO: Error if write other Than A B C D
+        """
+        abc = ['A', 'B', 'C', 'D']
 
-def save_db(db: list, filename='db1_short.csv'):
-    with open(filename, 'w') as file:
-        writer = csv.DictWriter(file, fieldnames=[i for i in db[0]])
-        writer.writeheader()
-        writer.writerows(db)
+        for i, row in enumerate(self.db):
+            print('-' * 80)
+            print(f'{i + 1}. {row["question"]}')
+            # print(row['choices'].split(', '))
+            choices = row['choices'].split(', ')
+            choices.append(row['answer'])
+            random.shuffle(choices)
+            choices = (dict(zip(abc, choices)))
 
+            for letter, choice in choices.items():
+                print(f'\t{letter}. {choice}')
 
-def freeform_question(db):
-    for i, row in enumerate(db):
-        print('-' * 80)
-        print(f'{i + 1}. {row["question"]}')
-        user_answer = input('Your answer: ')
-        if user_answer == row['answer']:
-            print('Success! Your answer is correct!')
-            row['correct'] += 1
-            row['times_shown'] += 1
-        else:
-            print(f'You are wrong. Correct answer: {row["answer"]}')
-            row['times_shown'] += 1
+            user_answer = input('Your answer: ')
+            if choices[user_answer] == row['answer']:
+                print('Success! Your answer is correct!')
+                row['correct'] += 1
+                row['times_shown'] += 1
+            else:
+                print(f'You are wrong. Correct answer: {row["answer"]}')
+                row['times_shown'] += 1
 
-    save_db(db)
-    # print(db)
+        self._save_db()
 
+    def add_question(self):
+        # question;multiple_choices;correct_answer
+        # if multipple_choices is omited, then it will be treated as
+        # free-form question instead of quiz with multiple choices
 
-def quiz_question(db):
-    abc = ['A', 'B', 'C', 'D']
+        # free-form example: Capital of Poland;;Warsaw
+        # quiz example: Capital of Poland;Vilnius, Riga, Kiev;Warsaw
 
-    for i, row in enumerate(db):
-        print('-' * 80)
-        print(f'{i + 1}. {row["question"]}')
-        # print(row['choices'].split(', '))
-        choices = row['choices'].split(', ')
-        choices.append(row['answer'])
-        random.shuffle(choices)
-        choices = (dict(zip(abc, choices)))
+        template = {
+            '_id': max(row['_id'] for row in self.db) + 1,
+            'status': 'active',
+            'question': '',
+            'choices': '',
+            'answer': '',
+            'times_shown': 0,
+            'correct': 0,
+        }
 
-        for letter, choice in choices.items():
-            print(f'\t{letter}. {choice}')
+        question = input('Your question: ')
+        question = question.split(';')
 
-        user_answer = input('Your answer: ')
-        if choices[user_answer] == row['answer']:
-            print('Success! Your answer is correct!')
-            row['correct'] += 1
-            row['times_shown'] += 1
-        else:
-            print(f'You are wrong. Correct answer: {row["answer"]}')
-            row['times_shown'] += 1
+        template['question'] = question[0]
+        template['choices'] = question[1]
+        template['answer'] = question[2]
 
-    save_db(db)
+        self.db.append(template)
+        self._save_db()
 
+    def reset(self):
+        for row in self.db:
+            row['correct'] = 0
+            row['times_shown'] = 0
+        self._save_db()
 
-def add_question(db, question):
-    # question;multiple_choices;correct_answer
-    # if multipple_choices is omited, then it will be treated as
-    # free-form question instead of quiz with multiple choices
+    def _get_question_index(self, qid:int) -> int:
+        index = 0
+        for i, row in enumerate(self.db):
+            if row['_id'] == int(qid):
+                index = i
+        return index
 
-    # free-form example: Capital of Poland;;Warsaw
-    # quiz example: Capital of Poland;Vilnius, Riga, Kiev;Warsaw
+    def _tabulate_data(self, rows:list) -> str:
+        table = tabulate(
+            rows,
+            headers='keys',
+            tablefmt='rounded_grid',
+            colalign=('right', 'center', 'left', 'left', 'left', 'center')
+        )
+        return table
 
-    template = {
-        'id': max([row['id'] for row in db]) + 1,
-        'status': 'active',
-        'question': '',
-        'choices': '',
-        'answer': '',
-        'times_shown': 0,
-        'correct': 0,
-    }
+    def question_status(self, qid:int) -> str:
+        qu_index = self._get_question_index(int(qid))
+        row = self.db[qu_index]
+        return self._tabulate_data([row])
 
-    question = question.split(';')
+    def toggle_question_status(self, qid:int) -> str:
+        qu_index = self._get_question_index(qid)
+        row = self.db[qu_index]
+        row['enabled'] = not row['enabled']
 
-    template['question'] = question[0]
-    template['choices'] = question[1]
-    template['answer'] = question[2]
+        self._save_db()
+        return self._tabulate_data([row])
 
-    db.append(template)
-    save_db(db)
+    def update_question(self, qid:int):
+        qu_index = self._get_question_index(qid)
+        row = self.db[qu_index]
 
+        question = input('Your question: ')
+        question = question.split(';')
 
-def status(db):
-    """
-    Show grid with questions and their status
-    """
+        row['question'] = question[0]
+        row['choices'] = question[1]
+        row['answer'] = question[2]
 
-    for row in db:
-        if row['choices']:
-            row['type'] = 'multi-choice'
-        else:
-            row['type'] = 'free-form'
+        self._save_db()
+        return self._tabulate_data([row])
 
-        del row['answer']
-        del row['choices']
+    def reset_question(self, qid:int):
+        qu_index = self._get_question_index(qid)
+        row = self.db[qu_index]
 
-        if row['correct'] == 0:
-            row['correct'] = '0%'
-        else:
-            row['correct'] = \
-                f'{row["times_shown"] / row["correct"] * 100}%'
+        row['enabled'] = True
+        row['times_shown'] = 0
+        row['correct'] = 0
 
-    # print(tabulate(db, headers='keys', tablefmt="rounded_outline"))
-    return tabulate(
-        db,
-        headers='keys',
-        tablefmt='rounded_grid',
-        colalign=('right', 'center', 'left', 'left', 'left', 'center')
-    )
+        self._save_db()
+        return self._tabulate_data([row])
 
+    def delete_question(self, qid:int):
+        qu_index = self._get_question_index(qid)
 
-def args():
-    pass
+        self.db.pop(qu_index)
+        self._save_db()
+
+    def status(self) -> str:
+        """
+        Show grid with questions and their status
+        """
+        for row in self.db:
+            if row['choices']:
+                row['type'] = 'multi-choice'
+            else:
+                row['type'] = 'free-form'
+
+            row.pop('answer')
+            row.pop('choices')
+
+            if row['correct'] == 0:
+                row['correct'] = '0%'
+            else:
+                row['correct'] = \
+                    f'{row["times_shown"] / row["correct"] * 100}%'
+
+        table = tabulate(
+            self.db,
+            headers='keys',
+            tablefmt='rounded_grid',
+            colalign=('right', 'center', 'left', 'left', 'left', 'center')
+        )
+        return table
 
 
 def main():
-    db = get_db('db1_short.csv')
-    # print(db)
+    args = docopt(__doc__, version='0.01')
 
-    # freeform_question(db)
-    # quiz_question(db)
+    q = Quiz('db1_short.csv')
 
-    # a = input('question: ')
-    # add_question(db, a)
+    if args['stats']:
+        print(q.status())
 
-    print(status(db))
+    if args['test']:
+        q.quiz_question()
+
+    # Questions manipulation
+    if args['question']:
+        if args['<id>']:
+            if args['--enabled']:
+                print(q.toggle_question_status(args['<id>']))
+            elif args['--update']:
+                q.update_question(args['<id>'])
+            elif args['--reset']:
+                q.reset_question(args['<id>'])
+            elif args['--remove']:
+                q.delete_question(args['<id>'])
+            else:
+                print(q.question_status(args['<id>']))
+
+    # reset
+    if args['reset']:
+        q.reset()
 
 
 if __name__ == '__main__':
@@ -196,3 +268,11 @@ if __name__ == '__main__':
 
 # Link to the public GitHub repo tha contains my work from Part 3:
 # TODO: https://github.com/viliusddd/turing/quiz
+
+
+"""
+TODO:
+1. Add -v switch
+2. Split to two classes: Questions and Quiz
+3. Add confirmation dialog, e.g. do you really want to reset all questions? yN
+"""
