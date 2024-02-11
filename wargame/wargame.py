78724a098e20dffaf8532b1567a55f34a0dfa339
Vilius <vilius@vilius.net>
2024-02-11 09:05:03 +0200

    Add war logic

--- wargame/wargame.py
+++ wargame/wargame.py
@@ -7,7 +7,7 @@ class Card:
     def __init__(self, color, value) -> None:
         self.color = color
         self.value = value
-        self.card = {color: value}
+        self.card = (value, color)
 
     def __str__(self):
         print(f'{self.value} of {self.suit}')
@@ -40,8 +40,9 @@ class Computer:
         return Computer.cards[-1]
 
 class War:
+    pile = []
 
-    def __init__(self, player, computer):
+    def __init__(self, player, computer) -> None:
         self.player = player
         self.computer = computer
 
@@ -50,21 +51,56 @@ class War:
             pass
 
 
-def main():
+def main() -> None:
     deck = Deck()
     deck = deck.cards
 
     player = Player()
     computer = Computer()
 
+    # deal 26 cards to each player
     player.cards, computer.cards = deck[::2], deck[1::2]
 
-    for i in enumerate(player.cards):
-        print(player.cards[-1], computer.cards[-1])
-        player.cards.pop(-1)
-        computer.cards.pop(-1)
+    # for i in enumerate(player.cards):
+    #     print(player.cards[-1], computer.cards[-1])
+    #     player.cards.pop(-1)
+    #     computer.cards.pop(-1)
 
     war = War(player, computer)
+
+    war_pile = []
+    win = 0
+    # flip cards
+    for pl, cp in (zip(reversed(player.cards), reversed(computer.cards))):
+
+        print(len(player.cards), pl, cp)
+        if not war_pile:
+            if pl[0] > cp[0]:
+                player.pile.extend(pl+cp)
+            elif pl[0] < cp[0]:
+                computer.pile.extend(pl+cp)
+            else:
+                war.pile.extend(pl+cp)
+        elif len(war_pile) % 2 == 0:
+            war_pile.extend(pl+cp)
+        elif len(war_pile) % 4 == 0:
+            if pl[0] > cp[0]:
+                player.pile.extend(cp+pl+war_pile)
+                war.pile = []
+            elif pl[0] < cp[0]:
+                computer.pile.extend(cp+pl+war_pile)
+                war.pile = []
+            else:
+                war_pile.extend(cp+pl)
+
+        del computer.cards[-1]
+        del player.cards[-1]
+
+        # when deck finishes and both players have cards in pile: transfer pile cards to deck and
+        # run loop again. Until one of the players won't have cards in both: deck and pile
+
+    print(player.pile)
+    print(computer.pile)
 
 
 if __name__ == '__main__':
