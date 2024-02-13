e239c35303b218390bbdeec21c3a16a558193f9b
Vilius <vilius@vilius.net>
2024-02-13 10:01:13 +0200

    Initial

--- wargame/wargame.py
+++ wargame/wargame.py
@@ -1,20 +1,25 @@
+'''
+2-4 player game of war. 2 players - 26 cards each, 3 - 17, 4 - 13.
+It should have optional names arg and arg for number of players. Or
+it should count the number of players by the number of names. name arg
+shoulbe be able to be repeated multiple times.
+The args option to play "peace" where the lowest card wins.
+
+'''
 import random
+import sys
 
 
 class Card:
-    colors = ['spades', 'clubs', 'hearts', 'diamonds']
+    COLORS = ['spades', 'clubs', 'hearts', 'diamonds']
 
     def __init__(self, color, value) -> None:
-        self.color = color
-        self.value = value
         self.card = (value, color)
 
-    def __str__(self):
-        print(f'{self.value} of {self.suit}')
 
 class Deck:
     def __init__(self) -> None:
-        self.cards = [Card(color, value).card for value in range(1, 14) for color in Card.colors]
+        self.cards = [Card(color, value).card for value in range(1, 14) for color in Card.COLORS]
         self.shuffle()
 
     def shuffle(self):
@@ -22,85 +27,77 @@ class Deck:
 
 
 class Player:
-    cards = []
-    pile = []
-    def __init__(self) -> None:
-        pass
-
-class Computer:
-    cards = []
-    pile = []
-    def __init__(self) -> None:
-        pass
-
-    @classmethod
-    def draw():
-        card = Computer.cards[-1]
-        del Computer.cards[card]
-        return Computer.cards[-1]
-
-class War:
     pile = []
 
-    def __init__(self, player, computer) -> None:
-        self.player = player
-        self.computer = computer
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self.cards = []
+        self.pile = []
 
-    def compare(self):
-        if self.player_card > self.computer_card:
-            pass
+    def draw_card(self) -> tuple:
+        '''Take a card from the top of the deck.
+
+        Returns:
+            tuple: with card value and color, e.g. (4, 'spades')
+        '''
+        if not self.cards:
+            sys.exit(f'No more cards left for {self.name}. Oponent won')
+
+        return self.cards.pop(0)
+
+    def add_cards(self, cards: list) -> None:
+        '''Add card to the bottom of the deck.
+
+        Args:
+            cards (list): with card value and color, e.g. [(4, 'spades')]
+        '''
+        self.cards.append(cards)
+
+        if Player.pile:
+            print(f'Cards left in pile: {len(Player.pile)}')
+            self.cards = Player.pile + self.cards
+            Player.pile = []
+
+    def cards_left(self):
+        return len(self.cards)
+
+
+def draw(p1: Player, p2: Player) -> None:
+    c1, c2 = p1.draw_card(), p2.draw_card()
+    print(f'{p1.name} "{c1[1]} {c1[0]}" vs {p2.name} "{c2[1]} {c2[0]}"')
+
+    if c1[0] > c2[0]:
+        p1.add_cards(c1)
+        p1.add_cards(c2)
+    elif c2[0] > c1[0]:
+        p2.add_cards(c2)
+        p2.add_cards(c1)
+    else:
+        print('Draw')
+        Player.pile.extend([c1] + [c2] + [p1.draw_card()] + [p2.draw_card()])
+
+    print(f'{p1.name} cards: {p1.cards_left()}, {p2.name} cards: {p2.cards_left()}')
+    print('-' * 50)
 
 
 def main() -> None:
     deck = Deck()
-    deck = deck.cards
 
-    player = Player()
-    computer = Computer()
+    player, computer = Player('Player'), Player('ClosedAI')
+    player.cards, computer.cards = deck.cards[::2], deck.cards[1::2]
 
-    # deal 26 cards to each player
-    player.cards, computer.cards = deck[::2], deck[1::2]
+    round = 1
+    while player.cards and computer.cards:
+        print(f'Round {round}:')
 
-    # for i in enumerate(player.cards):
-    #     print(player.cards[-1], computer.cards[-1])
-    #     player.cards.pop(-1)
-    #     computer.cards.pop(-1)
+        draw(player, computer)
 
-    war = War(player, computer)
+        round += 1
 
-    war_pile = []
-    win = 0
-    # flip cards
-    for pl, cp in (zip(reversed(player.cards), reversed(computer.cards))):
-
-        print(len(player.cards), pl, cp)
-        if not war_pile:
-            if pl[0] > cp[0]:
-                player.pile.extend(pl+cp)
-            elif pl[0] < cp[0]:
-                computer.pile.extend(pl+cp)
-            else:
-                war.pile.extend(pl+cp)
-        elif len(war_pile) % 2 == 0:
-            war_pile.extend(pl+cp)
-        elif len(war_pile) % 4 == 0:
-            if pl[0] > cp[0]:
-                player.pile.extend(cp+pl+war_pile)
-                war.pile = []
-            elif pl[0] < cp[0]:
-                computer.pile.extend(cp+pl+war_pile)
-                war.pile = []
-            else:
-                war_pile.extend(cp+pl)
-
-        del computer.cards[-1]
-        del player.cards[-1]
-
-        # when deck finishes and both players have cards in pile: transfer pile cards to deck and
-        # run loop again. Until one of the players won't have cards in both: deck and pile
-
-    print(player.pile)
-    print(computer.pile)
+        if not len(player.cards):
+            print(f'{computer.name} won.')
+        if not len(computer.cards):
+            print(f'{player.name} won.')
 
 
 if __name__ == '__main__':
